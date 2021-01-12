9b60379a1ba54bcbb26fe8956cde8aa3b9e27e2d
WizBash <53521065+WizBash@users.noreply.github.com>
2021-01-12 03:20:51 -0800

    Update SQLite3.py

--- SQLite3.py
+++ SQLite3.py
@@ -1,16 +1,19 @@
+#===============================WE FIRST IMPORT THE SQLITE3 MODULE ================
 import sqlite3
 
-
+#======================== WE USE TRY TO RAISE ERRORS INCASE THERE IS ANY======================
 try:
-
+	#========================creating and connecting to our databse====================
 	db=sqlite3.connect('pass.db')
 	conn=db.cursor()
+	
+	#===============================creating table in our databse=====================
 	conn.execute("""CREATE TABLE IF NOT EXISTS pass_table (
-		NAME TEXT,
-		AGE INT,
-    SEX TEXT,
-    JOB TEXT,
-    SALARY INT 
+	NAME TEXT,
+	AGE INT,
+	SEX TEXT, 
+	JOB TEXT,
+	SALARY INT 
 	)
 	""")
 	db.commit()
@@ -18,4 +21,7 @@ try:
 	print('Database Connection Successfull')
 
 except Exception as e:
-	print(e)
+	print(e) #this prints out the error if there is any
+	
+	
+#=============================inserting data in our database table=====================
